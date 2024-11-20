0584f28dcc2b84bd623d1f205091b24e7c4e2d7b
mattymo30 <msmorrison39@yahoo.com>
2024-11-20 21:12:46 +0000

    C and js commit

---
+++ my-c-file/my-c-f.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int myNum;
+    printf("hello world\n");
+
+    printf("type a number: \n");
+
+    scanf("%d", &myNum);
+
+    printf("The number %d is cool", myNum);
+}
